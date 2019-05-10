71aecc804534c775b79c9cfc3793b1f9fd6f6294
Sebastian Bohmann <mail@sebastian-bohmann.com>
2019-05-10 10:19:25 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ export JNI_OS=freebsd
 export MAGICK_INCLUDES=/usr/local/include/ImageMagick-7
 export MAGICK_LIBS=/usr/local/lib
 export MAGICK_WAND_LIBRARY=MagickWand-7
-ls -lah "$MAGICK_HOME/include"
+mkdir build
 (cd build; cmake ..; make clean; cmake --build .)
