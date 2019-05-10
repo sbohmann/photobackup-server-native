c1cf04a36ab7a717ee39ed5361cf12ba34e4c232
Sebastian Bohmann <mail@sebastian-bohmann.com>
2019-05-10 15:59:32 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@ export JAVA_HOME
 export JNI_OS
 export MAGICK_INCLUDES=/usr/local/include/ImageMagick-7
 export MAGICK_LIBS=/usr/local/lib
-export MAGICK_WAND_LIBRARY=MagickWand-7
+export MAGICK_WAND_LIBRARY
 mkdir build
 (cd build; cmake ..; make clean; cmake --build .)
