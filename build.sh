717603ff4279ecc2f1e46ec7b6fd660feb3db01d
Sebastian Bohmann <mail@sebastian-bohmann.com>
2019-05-10 11:27:07 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 export JAVA_HOME
-export JNI_OS=freebsd
+export JNI_OS
 export MAGICK_INCLUDES=/usr/local/include/ImageMagick-7
 export MAGICK_LIBS=/usr/local/lib
 export MAGICK_WAND_LIBRARY=MagickWand-7
